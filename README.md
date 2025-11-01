c1eb7f939187ef4a74eb35eb6aa2993b2659e1cd
Ayodeji Akenroye <e.manuel_07@yahoo.com>
2025-11-01 11:47:02 +0100

    Add working image resize task with Celery

---
+++ README.md
@@ -0,0 +1,7 @@
+To fix:
+fix resize path(return relative path or URL instead)
+
+Future Improvements:
+Add cropping options (center crop, smart crop)
+Support multiple output sizes
+Add image optimization (compression)
--- tasks/views.py
+++ tasks/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 
 from tasks.models import Job
 from tasks.serializers import JobSerializer
+from tasks.tasks import resize_image
 
 
 class JobView(APIView):
@@ -11,9 +12,8 @@ class JobView(APIView):
 
     def get(self, request, job_id):
         job = get_object_or_404(Job, id=job_id)
-        if job:
-            serializer = self.serializer_class(job)
-            return Response(status=status.HTTP_200_OK, data=serializer.data)
+        serializer = self.serializer_class(job)
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
 
     def post(self, request):
         # API endpoint receives image upload
@@ -21,12 +21,13 @@ class JobView(APIView):
 
         if serializer.is_valid():
             # Creates a Job record in database (status: PENDING)
-            serializer.save()
+            instance = serializer.save()
+            print("orginal", instance.original_image.path)
 
             # Triggers Celery task with job ID
+            resize_image.delay(instance.id, instance.original_image.path)
 
             return Response(status=status.HTTP_201_CREATED, data=serializer.data)
         return Response(status=status.HTTP_400_BAD_REQUEST, data=serializer.errors)
 
-        # Task resizes image, updates Job status to COMPLETED
         # API endpoint to check job status returns the result
